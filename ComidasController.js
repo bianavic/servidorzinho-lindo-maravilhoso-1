1dd7b4b8a6ffd967a1542a21a584876f747b5f20
bianavic <bianavic@gmail.com>
2019-06-11 22:08:55 -0300

    HTTP Methods ok

--- ComidasController.js
+++ ComidasController.js
@@ -1,61 +1,49 @@
 const { connect } = require('./ComidasRepository')
 const comidasModel = require('./ComidasSchema')
 
-connect() //para conectar no mongoDB
+connect() // para conectar no mongoDB
 
 const getAll = async () => {
-  // pegue todas as comidas do mongodb usando mongoose
-  // nome da collection: comidas
   return comidasModel.find((error, comidas) => {
     return comidas
   })
 }
 
 const getById = async (id) => {
-    return comidasModel.findById(id, (error, comida) => {
+  return comidasModel.findById(
+    id,
+    (error, comida) => {
       return comida
-    })
-}
-
-const add = (comida) => {
-//TODO: usar o mongoose para inserir uma nova comida
-const novaComida = new comidasModel({
-  nome: comida.nome,
-  descricao: comida.descricao
-})
-  novaComida.save()
-}
-
-const remove = (id) => {
-  comidas.pratosFavoritos = getAll().filter((comida) => {
-    return comida.id !== id
-  })
-}
-
-const update = (id, comida) => {
-  let comidaCadastrada = getAll().find(comida => {
-    return comida.id === id
-  })
-
-  if(comidaCadastrada === undefined){ // nao encontrou a comida
-    return false
-  }
-  else {
-    if(comida.nome !== undefined) {
-      comidaCadastrada.nome = comida.nome
     }
-    if(comida.descricao !== undefined) {
-      comidaCadastrada.descricao = comida.descricao
-    }
+  ) 
+}
 
-    return true
-  }
+const add = async (comida) => {
+  const novaComida = new comidasModel(comida)
+  return novaComida.save()
+}
+
+
+const remove = async (id) => {
+  return comidasModel.findByIdAndDelete(id)
+}
+
+const update = async (id, comida) => {
+  return comidasModel.findByIdAndUpdate(
+    id,
+    { $set: comida },
+    { new: true }, // RETORNAR A COMIDA JA ATUALIZADA NO CALLBACK
+    function (error, comida) { // é o nosso callback
+      return comida
+    }
+  )
+
 }
 
 module.exports = {
   getAll,
+  getById,
   add,
   remove,
-  update,
-  getById
+  update
 }
--- ComidasSchema.js
+++ ComidasSchema.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 //cada schema equivale collection
+
 const Schema = mongoose.Schema;
 const ComidasSchema = new Schema({
     _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
     nome: { type: String, required: true },
     descricao: { type: String } // required fica opcional
-
 })
 
-const comidas = mongoose.model("comidas", ComidasSchema);
+const comidasModel = mongoose.model("comidas", ComidasSchema);
 
-module.export = comidas;
+module.exports = comidasModel
